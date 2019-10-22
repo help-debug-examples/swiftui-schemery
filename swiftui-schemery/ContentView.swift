2f9ef65a1c754474987031c5b03ba7623ee1da73
Derrick Showers <derrick@derrickshowers.com>
2019-10-22 19:21:25 -0400

    Initial Commit

---
+++ swiftui-schemery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-schemery
+//
+//  Created by Derrick Showers on 10/22/19.
+//  Copyright © 2019 Derrick Showers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
